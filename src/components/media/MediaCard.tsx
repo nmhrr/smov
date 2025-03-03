d904b34b40492c29a65e3814a0a1ff1d7fcae06e
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-03-03 16:48:26 -0700

    revert hover rules

--- src/components/media/MediaCard.tsx
+++ src/components/media/MediaCard.tsx
@@ -141,9 +141,7 @@ function MediaCardContent({
     >
       <Flare.Base
         className={`group -m-[0.705em] rounded-xl bg-background-main transition-colors duration-300 focus:relative focus:z-10 ${
-          canLink && !overlayVisible
-            ? "hover:bg-mediaCard-hoverBackground tabbable"
-            : ""
+          canLink ? "hover:bg-mediaCard-hoverBackground tabbable" : ""
         }`}
         tabIndex={canLink ? 0 : -1}
         onKeyUp={(e) => e.key === "Enter" && e.currentTarget.click()}
@@ -158,11 +156,7 @@ function MediaCardContent({
         />
         <Flare.Child
           className={`pointer-events-auto relative mb-2 p-[0.4em] transition-transform duration-300 ${
-            canLink && !overlayVisible
-              ? isHoveringCard
-                ? "scale-95"
-                : ""
-              : "opacity-60"
+            canLink ? (isHoveringCard ? "scale-95" : "") : "opacity-60"
           }`}
         >
           <div
